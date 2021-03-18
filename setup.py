bece835e52ed8b012ee2f8f7258f92faf0987c60
firedm <mahmoud.aboghazala@gmail.com>
2021-03-18 14:30:15 +0200

    [dependencies] update awesometkinter minimum version to "2021.3.18"

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ try:
         requirements = fh.readlines()
 except:
     requirements = ['plyer', 'certifi', 'youtube_dl', 'yt_dlp', 'pycurl', 'pillow >= 6.0.0', 'pystray',
-                    'awesometkinter >= 2020.12.16']
+                    'awesometkinter >= 2021.3.18']
 
 setuptools.setup(
     name="FireDM",
