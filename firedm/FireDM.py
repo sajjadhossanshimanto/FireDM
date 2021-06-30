6d78b51c065f13ef8956ec5f66f0f21c38face2b
firedm <mahmoud.aboghazala@gmail.com>
2021-06-30 13:25:35 +0200

    update cpyright year to 2021

--- firedm/FireDM.py
+++ firedm/FireDM.py
@@ -4,7 +4,7 @@
 
     multi-connections internet download manager, based on "LibCurl", and "youtube_dl".
 
-    :copyright: (c) 2019-2020 by Mahmoud Elshahat.
+    :copyright: (c) 2019-2021 by Mahmoud Elshahat.
     :license: GNU LGPLv3, see LICENSE for more details.
 
     Module description:
--- firedm/video.py
+++ firedm/video.py
@@ -3,7 +3,7 @@
 
     multi-connections internet download manager, based on "LibCurl", and "youtube_dl".
 
-    :copyright: (c) 2019-2020 by Mahmoud Elshahat.
+    :copyright: (c) 2019-2021 by Mahmoud Elshahat.
     :license: GNU LGPLv3, see LICENSE for more details.
 """
 import copy
