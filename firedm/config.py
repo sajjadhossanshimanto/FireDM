aab810663ea8bb6d1f3a3bac0771c572610b2f74
firedm <mahmoud.aboghazala@gmail.com>
2021-07-01 18:34:13 +0200

    default checksum calculation to false

--- firedm/config.py
+++ firedm/config.py
@@ -180,7 +180,7 @@ on_completion_command = ''
 
 # debugging ------------------------------------------------------------------------------------------------------------
 keep_temp = False  # keep temp files / folders after done downloading for debugging
-checksum = True  # calculate checksums for completed files MD5 and SHA256
+checksum = False  # calculate checksums for completed files MD5 and SHA256
 use_thread_pool_executor = False
 max_seg_retries = 10  # maximum retries for a segment until reporting downloaded, this is for segment with unknown size
 
