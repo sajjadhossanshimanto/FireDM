73deeca239b4b2862a113a7c8f997f99761953f7
firedm <mahmoud.aboghazala@gmail.com>
2021-08-25 19:20:45 +0200

    organize code in config.py add variables under categories

--- firedm/config.py
+++ firedm/config.py
@@ -16,54 +16,29 @@ from .version import __version__
 
 # settings parameters to be saved on disk
 settings_keys = [
-    'current_theme', 'monitor_clipboard', 'speed_limit', 'max_concurrent_downloads', 'max_connections',
-    'update_frequency', 'last_update_check', 'proxy', 'enable_proxy', 'log_level', 'download_folder',
-    'manually_select_dash_audio', 'keep_temp', 'auto_rename', 'checksum', 'use_proxy_dns', 'write_metadata',
-    'check_for_update', 'minimize_to_systray', 'enable_systray', 'window_size', 'download_thumbnail',
-    'active_video_extractor', 'autoscroll_download_tab', 'enable_captcha_workaround', 'gui_font', 'scrollbar_width',
-    'verify_ssl_cert', 'custom_user_agent', 'recent_folders', 'use_playlist_numbers', 'refresh_url_retries',
-    'ditem_show_top', 'disable_log_popups', 'ibus_workaround', 'on_download_notification']
+    'active_video_extractor', 'auto_rename', 'autoscroll_download_tab', 'check_for_update', 'checksum', 'current_theme',
+    'custom_user_agent', 'disable_log_popups', 'ditem_show_top', 'download_folder', 'download_thumbnail',
+    'enable_captcha_workaround', 'enable_proxy', 'enable_systray', 'gui_font', 'ibus_workaround', 'keep_temp',
+    'last_update_check', 'log_level', 'manually_select_dash_audio', 'max_concurrent_downloads', 'max_connections',
+    'minimize_to_systray', 'monitor_clipboard', 'on_download_notification', 'proxy', 'recent_folders',
+    'refresh_url_retries', 'scrollbar_width', 'speed_limit', 'update_frequency', 'use_playlist_numbers',
+    'use_proxy_dns', 'verify_ssl_cert', 'window_size', 'write_metadata']
 
+# ----------------------------------------------------------------------------------------General ----------------------
 # CONSTANTS
 APP_NAME = 'FireDM'
 APP_VERSION = __version__
 APP_TITLE = f'{APP_NAME} version {APP_VERSION} .. an open source download manager'
-DEFAULT_DOWNLOAD_FOLDER = os.path.join(os.path.expanduser("~"), 'Downloads')
-DEFAULT_THEME = 'Dark'
-DEFAULT_CONNECTIONS = 10
 
-# minimum segment size which can be split in 2 halves in auto-segmentation process, refer to brain.py>thread_manager.
+# minimum segment size used in auto-segmentation process, refer to brain.py>thread_manager.
 SEGMENT_SIZE = 102400  # 100 KB
 
-DEFAULT_CONCURRENT_CONNECTIONS = 3
 APP_URL = 'https://github.com/firedm/FireDM'
 LATEST_RELEASE_URL = 'https://github.com/firedm/FireDM/releases/latest'
 
-# headers,
-DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3721.3'
-custom_user_agent = None
-
-#  a random user agent will be used later when importing youtube-dl, if no custom user agent
-http_headers = {
-    'User-Agent': custom_user_agent or DEFAULT_USER_AGENT,
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-us,en;q=0.5',
-}
-
-DEFAULT_LOG_LEVEL = 2
-
-youtube_dl_version = None
-yt_dlp_version = None
-atk_version = None  # awesometkinter
-
-TEST_MODE = False
-SIMULATOR = False
 FROZEN = getattr(sys, "frozen", False)  # check if app is being compiled by cx_freeze
-# -------------------------------------------------------------------------------------
 
-# current operating system  ('Windows', 'Linux', 'Darwin')
-operating_system = platform.system()
+operating_system = platform.system()  # current operating system  ('Windows', 'Linux', 'Darwin')
 
 # Example output: Os: Linux - Platform: Linux-5.11.0-7614-generic-x86_64-with-glibc2.32 - Machine: x86_64
 operating_system_info = f"Os: {platform.system()} - Platform: {platform.platform()} - Machine: {platform.machine()}"
@@ -83,7 +58,8 @@ appimage_update_folder = None  # will be set by AppImage run script
 # application exit flag
 shutdown = False
 
-# folders --------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------Filesystem options------------
+# current folders
 if hasattr(sys, 'frozen'):  # like if application frozen by cx_freeze
     current_directory = os.path.dirname(sys.executable)
 else:
@@ -94,12 +70,98 @@ sys.path.insert(0, current_directory)
 
 sett_folder = None
 global_sett_folder = None
-download_folder = DEFAULT_DOWNLOAD_FOLDER
-
+download_folder = os.path.join(os.path.expanduser("~"), 'Downloads')
 recent_folders = []
 
-# settings parameters
-# General --------------------------------------------------------------------------------------------------------------
+auto_rename = False  # auto rename file if there is an existing file with same name at download folder
+checksum = False  # calculate checksums for completed files MD5 and SHA256
+use_playlist_numbers = True  # add numbers to video file names when downloading thru playlist menu.
+
+# ---------------------------------------------------------------------------------------Network Options----------------
+proxy = ''  # must be string example: 127.0.0.1:8080
+enable_proxy = False
+use_proxy_dns = False
+
+# ---------------------------------------------------------------------------------------Authentication Options---------
+use_web_auth = False
+username = ''
+password = ''
+
+# ---------------------------------------------------------------------------------------Video Options------------------
+# youtube-dl abort flag, will be used by decorated YoutubeDl.urlopen(), see video.set_interrupt_switch()
+ytdl_abort = False
+video_extractors_list = ['youtube_dl', 'yt_dlp']
+active_video_extractor = 'youtube_dl'
+manually_select_dash_audio = False  # if True, will prompt user to select audio format for dash video
+
+ffmpeg_actual_path = None
+ffmpeg_download_folder = sett_folder
+
+video_quality = 'best'  # 'best', '1080p', '720p', '480p', '360p', and 'lowest'
+prefere_mp4 = True
+
+# ---------------------------------------------------------------------------------------Workarounds--------------------
+ibus_workaround = False  # issue 256: https://github.com/firedm/FireDM/issues/256
+verify_ssl_cert = True  # verify server's ssl certificate
+
+#  a random user agent will be used later when importing youtube-dl, if no custom user agent
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3721.3'
+custom_user_agent = None
+http_headers = {
+    'User-Agent': custom_user_agent or DEFAULT_USER_AGENT,
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-us,en;q=0.5',
+}
+
+use_referer = False
+referer_url = ''  # referer website url
+enable_captcha_workaround = False
+use_cookies = False
+cookie_file_path = ''
+
+# ---------------------------------------------------------------------------------------Post-processing Options--------
+download_thumbnail = False
+write_metadata = True  # write metadata to video file
+shutdown_pc = False
+on_completion_command = ''
+use_server_timestamp = False  # write 'last modified' timestamp to downloaded file
+
+# ---------------------------------------------------------------------------------------Application Update Options-----
+# set this flag to True to disable update feature completely
+disable_update_feature = False
+
+check_for_update = not disable_update_feature
+update_frequency = 7  # days
+last_update_check = None  # date format (year, month, day)
+
+youtube_dl_version = None
+yt_dlp_version = None
+atk_version = None  # awesometkinter
+
+# ---------------------------------------------------------------------------------------Downloader Options-------------
+refresh_url_retries = 1  # number of retries to refresh expired url when downloading a file, zero to disable
+speed_limit = 0  # in bytes, zero == no limit
+max_concurrent_downloads = 3
+max_connections = 10
+max_seg_retries = 10  # maximum retries for a segment until reporting downloaded, this is for segment with unknown size
+
+# ---------------------------------------------------------------------------------------Debugging options--------------
+keep_temp = False  # keep temp files / folders after done downloading for debugging
+
+log_entry = ''  # one log line
+max_log_size = 1024 * 1024 * 5  # 5 MB
+log_level = 2  # standard=1, verbose=2, debug=3
+
+# log callbacks that will be executed when calling log func in utils
+# callback and popup should accept 3 positional args e.g. log_callback(start, text, end)
+log_callbacks = []
+log_popup_callback = None
+TEST_MODE = False
+SIMULATOR = False
+
+# ---------------------------------------------------------------------------------------GUI options--------------------
+DEFAULT_THEME = 'Dark'
 current_theme = DEFAULT_THEME
 gui_font = {}
 gui_font_size_default = 10
@@ -109,100 +171,23 @@ scrollbar_width_default = 10
 scrollbar_width = scrollbar_width_default
 scrollbar_width_range = range(1, 51)
 monitor_clipboard = True
-auto_rename = False  # auto rename file if there is an existing file with same name at download folder
 autoscroll_download_tab = False
 ditem_show_top = True
-use_server_timestamp = False  # write 'last modified' timestamp to downloaded file
 
 # systray, it will be disabled by default since it doesn't work properly on most operating systems except Windows.
 enable_systray = True if operating_system == 'Windows' else False
 minimize_to_systray = False
 
-# issue 256: https://github.com/firedm/FireDM/issues/256
-ibus_workaround = False
-
-# gui window size
 DEFAULT_WINDOW_SIZE = (780, 433)  # width, height in pixels
 window_size = DEFAULT_WINDOW_SIZE
+on_download_notification = True  # show notify message when done downloading
 
-# show notify message when done downloading
-on_download_notification = True
-
-# video / audio --------------------------------------------------------------------------------------------------------
-# youtube-dl abort flag, will be used by decorated YoutubeDl.urlopen(), see video.set_interrupt_switch()
-ytdl_abort = False
-video_extractors_list = ['youtube_dl', 'yt_dlp']
-active_video_extractor = 'youtube_dl'
-download_thumbnail = False
-manually_select_dash_audio = False  # if True, will prompt user to select audio format for dash video
-write_metadata = True  # write metadata to video file
-enable_captcha_workaround = False
-use_playlist_numbers = True  # add numbers to video file names when downloading thru playlist menu.
-
-# ffmpeg
-ffmpeg_actual_path = None
-ffmpeg_download_folder = sett_folder
-
-# connection / network -------------------------------------------------------------------------------------------------
-speed_limit = 0  # in bytes, zero == no limit
-max_concurrent_downloads = DEFAULT_CONCURRENT_CONNECTIONS
-max_connections = DEFAULT_CONNECTIONS
-use_referer = False
-referer_url = ''  # referer website url
-verify_ssl_cert = True  # verify server's ssl certificate
-
-# website authentication
-use_web_auth = False
-username = ''
-password = ''
-
-# proxy
-proxy = ''  # must be string example: 127.0.0.1:8080
-enable_proxy = False
-use_proxy_dns = False
-
-# use_cookies
-use_cookies = False
-cookie_file_path = ''
-
-refresh_url_retries = 1  # number of retries to refresh expired url when downloading a file, zero to disable
-
-shutdown_pc = False
-on_completion_command = ''
-
-# debugging ------------------------------------------------------------------------------------------------------------
-keep_temp = False  # keep temp files / folders after done downloading for debugging
-checksum = False  # calculate checksums for completed files MD5 and SHA256
-max_seg_retries = 10  # maximum retries for a segment until reporting downloaded, this is for segment with unknown size
-
-# logging --------------------------------------------------------------------------------------------------------------
-log_entry = ''  # one log line
-max_log_size = 1024 * 1024 * 5  # 5 MB
-log_level = DEFAULT_LOG_LEVEL  # standard=1, verbose=2, debug=3
-
-# log callbacks that will be executed when calling log func in utils
-# callback and popup should accept 3 positional args e.g. log_callback(start, text, end)
-log_callbacks = []
-log_popup_callback = None
-# -------------------------------------------------------------------------------------
-
-video_quality = 'best'  # 'best', '1080p', '720p', '480p', '360p', and 'lowest'
-prefere_mp4 = True
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 
 # queues
 error_q = Queue()  # used by workers to report server refuse connection errors
 jobs_q = Queue()  # # required for failed worker jobs
-
-# update --------------------------------------------------------------------------------------------
-# set this flag to True to disable update feature completely
-disable_update_feature = False
-
-check_for_update = not disable_update_feature
-update_frequency = 7  # days
-last_update_check = None  # date format (year, month, day)
-
-
-# -----------------------------------------------------------------------------------------------------
 
 
 # status class as an Enum
