ec637b6fbacddf338e57704857b4e1149107414e
firedm <mahmoud.aboghazala@gmail.com>
2021-11-28 14:47:50 +0200

    remove reverse_playlist option

--- firedm/config.py
+++ firedm/config.py
@@ -23,7 +23,7 @@ settings_keys = [
     'username', 'password', 'max_connections', 'minimize_to_systray', 'monitor_clipboard', 'on_download_notification',
     'proxy', 'recent_folders', 'refresh_url_retries', 'scrollbar_width', 'speed_limit', 'update_frequency',
     'playlist_num_options', 'use_server_timestamp', 'window_size', 'write_metadata', 'view_mode',
-    'window_maximized', 'force_window_maximize', 'reverse_playlist', 'd_preview', 'updater_version', 'media_presets',
+    'window_maximized', 'force_window_maximize', 'd_preview', 'updater_version', 'media_presets',
 ]
 
 # ----------------------------------------------------------------------------------------General ----------------------
@@ -80,7 +80,6 @@ recent_folders = []
 auto_rename = False  # auto rename file if there is an existing file with same name at download folder
 checksum = False  # calculate checksums for completed files MD5 and SHA256
 playlist_num_options = {}
-reverse_playlist = False  # needed when playlist order is reversed
 
 # ---------------------------------------------------------------------------------------Network Options----------------
 proxy = ''  # must be string example: 127.0.0.1:8080
